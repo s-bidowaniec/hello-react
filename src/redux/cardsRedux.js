18eefa560a9f9141509cefe6d6f19cea101f5900
Stanisław Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2022-07-22 11:55:14 +0200

    Split reducer

---
+++ src/redux/cardsRedux.js
@@ -0,0 +1,16 @@
+import shortid from 'shortid';
+
+const cardsReducer = (statePart = [], action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      return [...statePart, { ...action.payload, id: shortid() }];
+    case 'TOGGLE_CARD_FAVORITE':
+      return statePart.map((card) =>
+        card.id === action.payload.id ? { ...card, isFavorite: !card.isFavorite } : card
+      );
+    default:
+      return statePart;
+  }
+};
+
+export default cardsReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
-import { createStore } from 'redux';
-import shortid from 'shortid';
+import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
 import strContains from '../utils/strContains';
-
-//selectors
+// reducers
+import listsReducer from './listsRedux';
+import columnsReducer from './columsRedux';
+import cardsReducer from './cardsRedux';
+import searchStringReducer from './searchStringRedux';
+// selectors
 export const getFilteredCards = ({ cards, searchString }, columnId) =>
   cards.filter((card) => card.columnId === columnId && strContains(card.title, searchString));
 export const getFavoriteCards = ({ cards }) => cards.filter((card) => card.isFavorite);
@@ -18,33 +21,17 @@ export const getSearchString = ({ searchString }) => searchString;
 export const addColumn = (payload) => ({ type: 'ADD_COLUMN', payload });
 export const addCard = (payload) => ({ type: 'ADD_CARD', payload });
 export const addList = (payload) => ({ type: 'ADD_LIST', payload });
-export const searchCard = (payload) => ({ type: 'SEARCH_CARD', payload });
-export const addToFavorite = (payload) => ({ type: 'ADD_TO_FAVORITE', payload });
+export const searchCard = (payload) => ({ type: 'UPDATE_SEARCHSTRING', payload });
+export const addToFavorite = (payload) => ({ type: 'TOGGLE_CARD_FAVORITE', payload });
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_COLUMN':
-      action.payload.id = shortid();
-      return { ...state, columns: [...state.columns, action.payload] };
-    case 'ADD_CARD':
-      action.payload.id = shortid();
-      return { ...state, cards: [...state.cards, action.payload] };
-    case 'SEARCH_CARD':
-      return { ...state, searchString: action.payload };
-    case 'ADD_LIST':
-      action.payload.id = shortid();
-      return { ...state, lists: [...state.lists, action.payload] };
-    case 'ADD_TO_FAVORITE':
-      return {
-        ...state,
-        cards: state.cards.map((card) =>
-          card.id === action.payload.id ? { ...card, isFavorite: !card.isFavorite } : card
-        )
-      };
-    default:
-      return state;
-  }
+const subreducers = {
+  lists: listsReducer,
+  columns: columnsReducer,
+  cards: cardsReducer,
+  searchString: searchStringReducer
 };
+
+const reducer = combineReducers(subreducers);
 
 const store = createStore(
   reducer,
